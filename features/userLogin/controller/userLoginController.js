7d64d7cc745bb5235cf390c99e9e102bde81ad8d
Devjoe <benagu477@gmail.com>
2024-09-14 12:03:50 +0100

    update now

--- features/userLogin/controller/userLoginController.js
+++ features/userLogin/controller/userLoginController.js
@@ -67,5 +67,98 @@ const LoginUser = async (req,res) => {
 
 }
 
+const uploadAgentFileForVerification = async (req,res,next) =>{
+    const  {verificationDocument}  = req.body
+  const user = UsersModel.findOne(req.user._id)
+   console.log(verificationDocument)
+    if(!verificationDocument){
+         res.status(403).json({
+           title:"Agent Verification Message",
+           status:403,
+           successfull:false,
+           message:'verificationDocument field is required to continue.'
 
-module.exports = {LoginUser}
+         })
+
+         return
+    }
+
+    if(verificationDocument.length<2){
+       res.status(403).json({
+         title:"Agent Verification Message",
+         status:403,
+         successfull:false,
+         message:'Two document is needed to be submitted check and try again.'
+
+       })
+
+       return
+  }
+
+     if(!verificationDocument.every((document)=>{
+       return document.base64 != null
+    })){
+       res.status(403).json({
+           title:"Agent Verification Message",
+           status:403,
+           successfull:false,
+           message:'You need base64 field to continue.'
+
+         })
+
+         return
+    }
+
+    if(!verificationDocument.every((document)=>{
+       var regexBase64 = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+       return  regexBase64.test(document.base64)
+    })){
+       res.status(403).json({
+           title:"Agent Verification Message",
+           status:403,
+           successfull:false,
+           message:'Invalid base64 provided please check and try again.'
+
+         })
+
+         return
+    }
+
+    try {
+       const folderPath = req.user._id
+       const  documentOne = await UploadFile(verificationDocument[0],folderPath)
+        const documentTwo = await UploadFile(verificationDocument[1],folderPath)
+   
+         Promise.all([documentOne,documentTwo]).then((result) => {
+             console.log(result)
+             res.status(200).json({
+               title:"Agent Verification Message",
+               status:200,
+               successfull:true,
+               message:'Successfully uploaded image.'
+   
+             })
+         }).catch((err) => {
+           
+           res.status(500).json({
+               title:"Agent Verification Message",
+               status:500,
+               successfull:false,
+               message:'error occured.',
+               error:err.message
+   
+             })
+         });
+       
+    } catch (error) {
+       next(error)
+    }
+    
+     
+
+    
+}
+
+
+
+module.exports = {LoginUser,uploadAgentFileForVerification}
--- features/userLogin/route/userLoginRoute.js
+++ features/userLogin/route/userLoginRoute.js
@@ -1,7 +1,12 @@
-const { LoginUser } = require('../controller/userLoginController')
+const authHandlerMiddleWare = require('../../../middleware/authHandlerMiddleWare')
+const { LoginUser, uploadAgentFileForVerification } = require('../controller/userLoginController')
+
+
 
 const route = require('express').Router()
 
 route.post('/loginUser',LoginUser)
+route.post('/verify-agent-documment',authHandlerMiddleWare,uploadAgentFileForVerification)
+
 
  module.exports = route
